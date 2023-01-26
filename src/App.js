c4d382c011f00bff22a93c5065dca498eb0c0e47
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-26 19:55:48 -0300

    Adicionado Componentes com Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  
+  var nome = "Manoportho"
   return (
     <div className="App">
-      <HelloWorld />   
+      <HelloWorld /> 
+      <SayMyName nome="Portho" />  
+      <SayMyName nome="Maria" />  
+      <SayMyName nome={nome} /> 
+      <Pessoa
+       nome="Portho"
+      idade="28" 
+      profissao="Programador" 
+      foto="https://via.placeholder.com/150"
+       />
     </div>
   );
 }
