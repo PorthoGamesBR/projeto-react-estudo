5ceb936a205b9eeafc724b7b44d3cd20140b1902
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-26 19:40:25 -0300

    Mudanças de teste no App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
+function sum(a, b){
+  return a + b;
+}
+
+const url = 'https://via.placeholder.com/150'
+
 function App() {
+  const name = 'Portho'
+
   return (
     <div className="App">
       <h1>Olá React</h1>
-      <p>Meu primeiro app</p>
+      <h2>Meu nome é {name}</h2>
+      <p>Este é meu primeiro app em react</p>
+      <p>A soma de 2+2 é {sum(2,2)}</p>
+      <img src={url} alt='Minha imagem'></img>     
     </div>
   );
 }
