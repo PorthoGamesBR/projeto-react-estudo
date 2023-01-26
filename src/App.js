a04c16cf665642003e5e0fdca48d5874363e6c1f
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-26 19:47:40 -0300

    Limpando Classe Principal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 
-function sum(a, b){
-  return a + b;
-}
-
-const url = 'https://via.placeholder.com/150'
-
 function App() {
-  const name = 'Portho'
-
+  
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <h2>Meu nome é {name}</h2>
-      <p>Este é meu primeiro app em react</p>
-      <p>A soma de 2+2 é {sum(2,2)}</p>
-      <img src={url} alt='Minha imagem'></img>  
       <HelloWorld />   
     </div>
   );
