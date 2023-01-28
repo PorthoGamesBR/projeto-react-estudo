f4ee52b9ed9c59b92254c1172fa913d4f97c9842
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-28 00:49:20 -0300

    Adicionados icones no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,15 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from "./Footer.module.css"
+ 
 function Footer(){
     return (
-        <h3>This is a footer</h3>
+        <>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+        </>
     )
 }
 
