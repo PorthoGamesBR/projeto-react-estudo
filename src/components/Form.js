e3747d9412b45b44a55b265712ad3b12ba2d6c8f
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-27 23:41:35 -0300

    Adicionando states e pegar dados dos componentes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,38 @@
+import { useState } from 'react'
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Cadastrou o usuário!")
+        console.log(`Usuario ${name} foi cadastrado com senha ${password}!`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name">Nome:</label>  
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite o seu nome" 
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>             
+                <div>
+                    <label htmlFor="senha">Senha:</label>
+                    <input 
+                        type="password"
+                        id="senha" 
+                        name="senha"
+                        placeholder="Digite a sua senha" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
