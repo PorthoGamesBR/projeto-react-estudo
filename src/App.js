eaef5abd4cfc78302d3dcd4eecbcea38848a7580
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-28 00:11:35 -0300

    Renderização de lista e condicional de lista

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
+import OutraLista from './components/OutraLista';
 //import HelloWorld from './components/HelloWorld';
 //import SayMyName from './components/SayMyName';
 //import Pessoa from './components/Pessoa';
 //import List from './components/List';
 //import Evento from './components/Evento';
 //import Form from './components/Form';
-import Conditional from './components/Condicional';
+//import Conditional from './components/Condicional';
 
 function App() {
   //var nome = "Manoportho"
+  const meusItems = ['React','Vue','Angular']
+
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Conditional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItems} />
+      <OutraLista itens={[]} />
     </div>
   );
 }
