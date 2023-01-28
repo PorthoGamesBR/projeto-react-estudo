71573137d86ca9e64d68a6a9eb6972a6fcaad714
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-27 23:47:56 -0300

    Adicionado metodos por props de componentes

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,11 +1,13 @@
+import Button from "./evento/Button"
+
 function Evento({ numero }){
     function meuEvento(){
-        console.log(`Evento ativado! Fui ativado com numero ${numero}`)
+        console.log(`Ativando primeiro evento! Numero ${numero}`)
     }
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar</button>
+            <Button event={meuEvento} text="Primeiro evento"></Button>
         </div>
     )
 }
